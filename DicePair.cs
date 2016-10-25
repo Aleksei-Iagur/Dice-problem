4876e1d6e383e762d698d17b7c2b252a52f0a382
Alex Yagur <asyagur@gmail.com>
2016-10-26 00:02:46 +0300

    Added DicePair

---
+++ DicePair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_problem
+{
+    struct DicePair
+    {
+        public Die Die1 { get; set; }
+        public Die Die2 { get; set; }
+
+        public bool Contains(Die die)
+        {
+            return Die1.Equals(die) || Die2.Equals(die);
+        }
+    }
+}
