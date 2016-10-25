7dd6a3494024e186dea1dd76d39f55927befa665
Alex Yagur <asyagur@gmail.com>
2016-10-26 00:03:27 +0300

    Added Probabilities

---
+++ Probabilities.cs
@@ -0,0 +1,12 @@
+﻿namespace Dice_problem
+{
+    class Probabilities
+    {
+        internal int[] Values { get; set; }
+
+        public void Reset()
+        {
+            Values = new[] { 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1 };
+        }
+    }
+}
